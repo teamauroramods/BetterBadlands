e4d220d5f8869d32fc0ece287df765b3ad5f012c
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2022-03-18 14:46:59 -0400

    Add assets, make properties class

--- common/src/main/java/com/teamaurora/better_badlands/core/registry/BetterBadlandsBlocks.java
+++ common/src/main/java/com/teamaurora/better_badlands/core/registry/BetterBadlandsBlocks.java
@@ -4,8 +4,11 @@ import com.teamaurora.better_badlands.core.BetterBadlands;
 import gg.moonflower.pollen.api.registry.PollinatedRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
 import java.util.function.Supplier;
 
@@ -13,10 +16,12 @@ public class BetterBadlandsBlocks {
     public static final PollinatedRegistry<Block> BLOCKS = PollinatedRegistry.create(Registry.BLOCK, BetterBadlands.MOD_ID);
 
 
-
     private static Supplier<Block> registerBlock(String id, Supplier<Block> block, Item.Properties properties) {
         Supplier<Block> register = BLOCKS.register(id, block);
         BetterBadlandsItems.ITEMS.register(id, () -> new BlockItem(register.get(), properties));
         return register;
+    }
+
+    public static final class Properties {
     }
 }
